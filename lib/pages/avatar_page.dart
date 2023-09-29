3b83867acb60d5b9b724500ff6b676c7ece035c5
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-29 07:09:14 -0500

    Avatar Page

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -8,6 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        centerTitle: true,
+        elevation: 5.0,
+        backgroundColor: Colors.amberAccent,
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg"),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage("assets/images/loading.gif"),
+            image: NetworkImage(
+                "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg"),
+            height: 300,
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 1600),
+          ),
+        ],
       ),
     );
   }
