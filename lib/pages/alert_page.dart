c099fb2e92b1c8dd315e549cc458023caafef354
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-29 07:28:35 -0500

    Alert dialog

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-  const AlertPage({super.key});
+  showMyAlert(BuildContext context1) {
+    showDialog(
+        context: context1,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Titulo del Alert 1"),
+            backgroundColor: Colors.white,
+            content: Text("Este es el contenido del alter 1"),
+            actions: [
+              Text("Si"),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("No"),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.check_box),
+              ),
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Alert Page"),
+        backgroundColor: Colors.black45,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                showMyAlert(context);
+              },
+              child: Text("Alert 1"),
+            )
+          ],
+        ),
       ),
     );
   }
